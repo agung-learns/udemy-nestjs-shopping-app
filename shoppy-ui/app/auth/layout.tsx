bd55c8e90123012ef072727610d4f841eaced6d6
agunggapai <agung@gapai.id>
2024-07-28 17:08:58 +0700

    [Section 2] Initialize Backend

---
+++ shoppy-ui/app/auth/layout.tsx
@@ -0,0 +1,9 @@
+import {Box} from "@mui/material";
+
+export default function AuthLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <Box className="h-screen flex items-center justify-center">
+            {children}
+        </Box>
+    )
+}
---
+++ shoppy-ui/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad aut consectetur culpa cum cupiditate dolorem dolores ducimus eaque error esse et labore laborum nisi porro reprehenderit, sed similique ullam.</p>
+    </main>
+  );
+}
